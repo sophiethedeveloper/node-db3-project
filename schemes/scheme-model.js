341791b9cf53ec18ca2466f357b7d61833edebff
sofia Luna <Sofia21luna@gmail.com>
2020-11-04 15:42:43 -0600

    find steps is working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -18,8 +18,13 @@ function findById(id) {
     return db('schemes').where({id}).first()
 }
 
-function findSteps() {
-    return console.log('wired')
+function findSteps(id) {
+    return db('schemes as sc')
+    .join('steps as st', 'sc.id', 'st.scheme_id')
+    .select('sc.id', 'sc.scheme_name', 'st.step_number', 'st.instructions' )
+    .where({'sc.id': id})
+    .orderBy('st.step_number')
+    
 }
 
 function add() {
