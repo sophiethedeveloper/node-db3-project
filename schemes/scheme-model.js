b656148f67cd180aa268542aac009492f6ec9efc
sofia Luna <Sofia21luna@gmail.com>
2020-11-04 15:07:18 -0600

    get all and get by id endpoints are working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,36 @@
 // scheme-model
+const db = require("../data/db-config");
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes').where({id}).first()
+}
+
+function findSteps() {
+    return console.log('wired')
+}
+
+function add() {
+    return console.log('wired')
+}
+
+function update() {
+    return console.log('wired')
+}
+
+function remove() {
+    return console.log('wired')
+}
+
